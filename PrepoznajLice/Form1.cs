dfc3471ab6e85fbc77798ca836d30b3faf88df68
Fr1k1 <martin.friscic6@gmail.com>
2022-11-18 16:39:22 +0100

    Add project files.

---
+++ PrepoznajLice/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FaceRecognition;
+
+namespace PrepoznajLice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FaceRec faceRec = new FaceRec();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            faceRec.openCamera(pictureBox1,pictureBox2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            faceRec.Save_IMAGE(textBox1.Text);
+            MessageBox.Show("Uspjesno spremljena slika");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            faceRec.isTrained = true;
+        }
+    }
+}
